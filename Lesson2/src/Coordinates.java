92f36c9bb0fc6fe3da86a60d9aec7aec7e34d866
Andrewdn74 <avakulenko@ukr.net>
2022-06-10 12:24:23 +0300

    Lesson2

---
+++ Lesson2/src/Coordinates.java
@@ -0,0 +1,9 @@
+public class Coordinates {
+    public static void main(String[] args) {
+        double latitude = 48.8582487759147d;
+        double longitude = 2.2945180844931d;
+        System.out.println("Eiffel tower coordinates: ");
+        System.out.println("Latitude = " + latitude + ", longitude = " + longitude);
+
+    }
+}
---
+++ Lesson2/src/Parallelepiped.java
@@ -0,0 +1,12 @@
+public class Parallelepiped {
+    public static void main(String[] args) {
+        int width = 5;
+        int incLength = 4;
+        int height = 8;
+        int volume = width * incLength * height;
+        int length =4 * (width + incLength + height);
+        System.out.println("Volume = " + volume);
+        System.out.println("Length = " + length);
+    }
+
+}
